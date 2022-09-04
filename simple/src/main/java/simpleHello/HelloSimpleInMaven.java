459bae366adc069fcfde735577c1a4de7f863651
SuyogWaghmare <suyogw0107@gmail.com>
2022-09-04 08:50:44 +0530

    First code in maven and git ex
    My first code is done for in maven and git thank you

---
+++ simple/src/main/java/simpleHello/HelloSimpleInMaven.java
@@ -0,0 +1,12 @@
+package simpleHello;
+
+public class HelloSimpleInMaven {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello this is simple maven and git example project");
+		System.out.println("Hello suyog");
+
+	}
+
+}
