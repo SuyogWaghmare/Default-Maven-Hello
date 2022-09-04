75c72aa9182c5e9d5ebb6477e71b9b5032408f3e
SuyogWaghmare <suyogw0107@gmail.com>
2022-09-04 09:40:30 +0530

    Second update is done in first updae
    Second update is done in first updae

---
+++ simple/src/main/java/myPackage1/SuyogFirstUpdate.java
@@ -0,0 +1,12 @@
+package myPackage1;
+
+public class SuyogFirstUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is suyog second branch for update new feacture "
+				+ "Thank you.....");
+	}
+
+}
